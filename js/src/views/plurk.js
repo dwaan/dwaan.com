daff21eb6b93d18f5e8d0f10e37b5af43fe94dba
Dwan <me@dwaan.com>
2020-12-14 15:18:12 +0200

    [ALL] Exluding private on most responses, adding permalink

--- js/src/views/plurk.js
+++ js/src/views/plurk.js
@@ -348,9 +348,22 @@ var plurk = {
 					</a>\
 				'));
 			},
-			drawPost: function(style, link, title, text, badge) {
+			drawPost: function(style, id, title, text, badge) {
+				var url = ""
+				if(id) url = 'https://plurk.com/p/' + id.toString(36);
 				this.el.insertAdjacentHTML('beforeend', this.wrapper(style, '\
 					<div>\
+						<a href="' + url + '" class="link" target="_BLANK">\
+							<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" width="24px" height="24px" viewBox="0 0 24 24" style="enable-background:new 0 0 24 24;" xml:space="preserve">\
+								<g id="Icons">\
+									<g id="link">\
+										<path id="right" d="M19.188,12.001c0,1.1-0.891,2.015-1.988,2.015l-4.195-0.015C13.543,15.089,13.968,16,15.002,16h3    C19.658,16,21,13.657,21,12s-1.342-4-2.998-4h-3c-1.034,0-1.459,0.911-1.998,1.999l4.195-0.015    C18.297,9.984,19.188,10.901,19.188,12.001z"/>\
+										<path id="center" d="M8,12c0,0.535,0.42,1,0.938,1h6.109c0.518,0,0.938-0.465,0.938-1c0-0.534-0.42-1-0.938-1H8.938    C8.42,11,8,11.466,8,12z"/>\
+										<path id="left" d="M4.816,11.999c0-1.1,0.891-2.015,1.988-2.015L11,9.999C10.461,8.911,10.036,8,9.002,8h-3    c-1.656,0-2.998,2.343-2.998,4s1.342,4,2.998,4h3c1.034,0,1.459-0.911,1.998-1.999l-4.195,0.015    C5.707,14.016,4.816,13.099,4.816,11.999z"/>\
+									</g>\
+								</g>\
+							</svg>\
+						</a>\
 						<span class="big">' + badge + '</span>\
 						<p class="post">' + text + '</p>\
 						<span class="title">' + title + '</span>\
@@ -649,8 +662,14 @@ var plurk = {
 					return b.response_count - a.response_count;
 				},
 				draw: function(data) {
+					var index = 0;
 					data.sort(this.sort);
-					if(data) statistics.drawPost('postcontent span2', '', '<i>Most Responded</i> ' + datediff(data[0].posted), data[0].content, data[0].response_count);
+					if(data) {
+						while(data[index].plurk_type == 3 && index <= data.length - 1) {
+							index++;
+						}
+						statistics.drawPost('postcontent span2', data[index].plurk_id, '<i>Most Responded</i> ' + datediff(data[index].posted), data[index].content, data[index].response_count);
+					}
 				}
 			},
 			replurk: {
@@ -659,7 +678,7 @@ var plurk = {
 				},
 				draw: function(data) {
 					data.sort(this.sort);
-					if(data) if(data[0].replurkers_count > 0) statistics.drawPost('postcontent span2', '', '<i>Most Replurked</i> ' + datediff(data[0].posted), data[0].content, data[0].replurkers_count);
+					if(data) if(data[0].replurkers_count > 0) statistics.drawPost('postcontent span2', data[0].plurk_id, '<i>Most Replurked</i> ' + datediff(data[0].posted), data[0].content, data[0].replurkers_count);
 				}
 			},
 			favorite: {
@@ -667,8 +686,14 @@ var plurk = {
 					return b.favorite_count - a.favorite_count;
 				},
 				draw: function(data) {
+					var index = 0;
 					data.sort(this.sort);
-					if(data) if(data[0].favorite_count > 0)  statistics.drawPost('postcontent span2', '', '<i>Most Favorited</i> ' + datediff(data[0].posted), data[0].content, data[0].favorite_count);
+					if(data) {
+						while(data[index].plurk_type == 3 && index <= data.length - 1) {
+							index++;
+						}
+						if(data[index].favorite_count > 0) statistics.drawPost('postcontent span2', data[index].plurk_id, '<i>Most Favorited</i> ' + datediff(data[index].posted), data[index].content, data[index].favorite_count);
+					}
 				}
 			}
 		};
@@ -677,7 +702,7 @@ var plurk = {
 			draw: function(data, year) {
 				this.year = year;
 				statistics.draw('inactive', year, 'You\'ve been inactive since ' + year + ' <img src="https://s.plurk.com/emoticons/platinum/318416eab5a856bddb1e106a21ff557a.gif" />');
-				statistics.drawPost('postcontent span2', '', '<i>Your last Plurk</i> ' + datediff(data[0].posted), data[0].content, data[0].response_count);
+				statistics.drawPost('postcontent span2', data[0].plurk_id, '<i>Your last Plurk</i> ' + datediff(data[0].posted), data[0].content, data[0].response_count);
 			},
 			empty: function() {
 				statistics.draw('inactive', '-', 'You haven\t posted anything at all <img src="https://s.plurk.com/emoticons/platinum/318416eab5a856bddb1e106a21ff557a.gif" />');
