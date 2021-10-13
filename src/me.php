63e185a82d05005d6014abe32f324bc32c64b671
Dwan <me@dwaan.com>
2021-10-13 13:58:01 +0300

    [ALL] Fixing layout hickupp in about me page

--- src/me.php
+++ src/me.php
@@ -1,64 +1,3 @@
-<?php
-/*
-	$secretKey = "MrGoatInDwanDotCom";
-	$configFile = "ig-config.txt";
-	$cacheFile = "ig-post.txt";
-
-	// Get long lived token from config
-	$igFile = fopen($configFile, "r") or die("Can't read config file");
-	$longLivedToken = openssl_decrypt(fread($igFile, filesize($configFile)), "AES-128-ECB", $secretKey);
-	fclose($igFile);
-	// Get new long lived token
-	$getdata = http_build_query(
-		array(
-			'grant_type' => 'ig_refresh_token',
-			'access_token' => $longLivedToken
-		)
-    );
-	$ch = curl_init('https://graph.instagram.com/refresh_access_token?' . $getdata);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	$response = curl_exec($ch);
-	curl_close($ch);
-
-	if (file_exists($cacheFile)) {
-		// Read post from file if exist
-		$igFile = fopen($cacheFile, "r");
-		$json = openssl_decrypt(fread($igFile, filesize($cacheFile)), "AES-128-ECB", $secretKey);
-		$obj = json_decode($json);
-		fclose($igFile);
-
-		// Check if the file is reacing 60 days, delete it to create a new one
-		$cache_life = 30*24*60*60;
-		$filemtime = @filemtime($cacheFile);
-		if (!$filemtime or (time() - $filemtime >= $cache_life)){
-			// Delete file
-			unlink($cacheFile);
-		}
-	} else {
-		// Rewrite the long lived token again before it expired
-		$obj = json_decode($response);
-		if ($obj->error->type == "IGApiException") {
-			die($obj->error->message);
-		} else {
-			$igFile = fopen($configFile, "w") or die("Can't write config file");
-			$encrypted_string = openssl_encrypt($obj->access_token, "AES-128-ECB", $secretKey);
-			fwrite($igFile, $encrypted_string);
-			fclose($igFile);
-		}
-
-		// Get newest post form IG
-		ini_set("allow_url_fopen", 1);
-		$json = file_get_contents("https://graph.instagram.com/me/media?fields=media_type,thumbnail_url,media_url,permalink,caption,permalink&access_token=" . $longLivedToken);
-		$obj = json_decode($json);
-
-		// Write newest post as a file
-		$igFile = fopen($cacheFile, "w") or die("Can't write config file");
-		$encrypted_string = openssl_encrypt($json, "AES-128-ECB", $secretKey);
-		fwrite($igFile, $encrypted_string);
-		fclose($igFile);
-	}
-*/
-?>
 <!doctype html>
 <html class="no-js" lang="en">
 
@@ -184,25 +123,9 @@
 			</div>
 		</section>
 
-		<!-- <section class="igstage middle">
-			<div class="thumbs">
-				<?php
-					$length = 12;
-					for ($i = 0; $i < $length; $i++) {
-						$data = $obj->data[$i];
-						if($data->media_type == "IMAGE" || $data->media_type == "CAROUSEL_ALBUM") {
-							echo "<a href='" . $data->permalink . "' data-barba-prevent target='BLANK' rel='noopener noreferrer'><img src='" . $data->media_url . "' alt='" . $data->caption . "' /></a>";
-						} elseif($data->media_type == "VIDEO") {
-							echo "<a href='" . $data->permalink . "' data-barba-prevent target='BLANK' rel='noopener noreferrer'><img src='" . $data->thumbnail_url . "' alt='" . $data->caption . "' /></a>";
-						}
-					}
-				?>
-			</div>
-		</section> -->
-
 		<div id="cofound" class="cofound middle clip">
 			<div class="text">
-				<h2>and  10 Years Ago</h2>
+				<h2>and 10 Years Ago</h2>
 				<p>My friend and I decide to create Sagara, an I.T. Solutions company based in Jakarta, where I work there full-time since 2010 until I moved to Tel Aviv. I still own small part of the company and act as their business partner right now.</p>
 				<a href="http://sagara.id" class="spring" data-barba-prevent target="BLANK" rel='noopener noreferrer'>More about sagara.id</a>
 			</div>
