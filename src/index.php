75f77e8e8a03b0062a729844a637de8de106bb52
Dwan <me@dwaan.com>
2022-08-15 16:08:59 +0300

    [ALL] Change all v1 to https

--- src/index.php
+++ src/index.php
@@ -92,8 +92,8 @@
 
         <section class="middle full home-old">
             <div class="padding">
-                <a href="http://v1.dwaan.com" class="dwantitle" aria-label="Dwan logo" data-barba-prevent>
-                    <svg class="diff" viewBox="0 0 33 51" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <a href="https://v1.dwaan.com" class="dwantitle" aria-label="Dwan logo" data-barba-prevent>
+                    <svg class="diff" viewBox="0 0 33 51" fill="none" xmlns="https://www.w3.org/2000/svg">
                         <g fill="white">
                             <g class="right-hand">
                                 <rect x="25.4991" y="39.1853" width="16" height="3.35392" rx="1.67696" transform="rotate(-135 25.4991 39.1853)"></rect>
@@ -115,7 +115,7 @@
                 </a>
             </div>
             <div class="arrow-small">
-                <a href="http://v1.dwaan.com" class="arrow" aria-label="Previous version of dwaan.com" data-barba-prevent>
+                <a href="https://v1.dwaan.com" class="arrow" aria-label="Previous version of dwaan.com" data-barba-prevent>
                     <span>Previous version</span>
                 </a>
             </div>
@@ -126,7 +126,7 @@
         <section class="flares">
             <?php
             for ($i = 1; $i <= 5; $i++) {
-                echo '<img src="/dwaan/img/flares/flare' . $i . '.webp" width="" height="" alt="" class="flare flare' . $i . '" />';
+                echo '<img src="/dwaan/img/flares/flare' . $i . '.webp" width="" height="" alt="Dwan\'s flare number' . $i . '" class="flare flare' . $i . '" />';
             }
             ?>
         </section>
@@ -135,7 +135,7 @@
             <a href="./say-hi" class="email" title="me@dwaan.com" aria-label="me@dwaan.com">me@dwaan.com</a>
             <div class="location">
                 <span>Jakarta</span>
-                <svg id="airplane" width="16" height="16" viewBox="0 0 16 16" fill="white" xmlns="http://www.w3.org/2000/svg">
+                <svg id="airplane" width="16" height="16" viewBox="0 0 16 16" fill="white" xmlns="https://www.w3.org/2000/svg">
                     <path d="M4.79998 1.59998L6.25415 6.93331H2.88019L1.59998 4.79998H1.06665V7.46664V9.06664V11.2H1.59998L2.88019 9.06664H6.25415L4.79998 14.4H5.86665L9.74582 9.06664H13.8666C14.456 9.06664 14.9333 8.58931 14.9333 7.99998C14.9333 7.41064 14.456 6.93331 13.8666 6.93331H9.74582L5.86665 1.59998H4.79998Z" />
                 </svg>
                 <span>Tel Aviv-Yafo</span>
