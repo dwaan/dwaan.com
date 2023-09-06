2f04676ced6bc803cd3d38a1159adf32b6c82713
Dwan <me@dwaan.com>
2023-09-06 11:42:23 +0300

    Moving print css to header

--- part_script.php
+++ part_script.php
@@ -1,6 +1,6 @@
 <?php
 // check for the server side x-barba request header
 if (!isset($_SERVER['HTTP_X_BARBA'])) {
-?> <link rel="stylesheet" media="print" href="/dwaan/css/print.css?<?php echo filemtime('css/print.css') ?>"> <?php
+?> <?php
 }
 ?>
--- src/part_script.php
+++ src/part_script.php
@@ -1,8 +1,4 @@
 <?php
 // check for the server side x-barba request header
 if (!isset($_SERVER['HTTP_X_BARBA'])) {
-?>
-	<link rel="stylesheet" media="print" href="/dwaan/css/print.css?<?php echo filemtime('css/print.css') ?>">
-<?php
 }
-?>
